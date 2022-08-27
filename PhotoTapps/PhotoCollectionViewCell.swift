2d50dad4e0ba0a9f72d8c7123eb3c13b51955865
DenysIlnytskyi <380984511271k@gmail.com>
2022-08-27 13:43:51 +0200

    Adding photos and writing logic for  CollectionView

---
+++ PhotoTapps/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoTapps
+//
+//  Created by Денис Ільницький on 27/08/2022.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    
+}
